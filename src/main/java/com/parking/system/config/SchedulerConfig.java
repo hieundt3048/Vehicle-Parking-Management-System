c9ca016e4b8fb5b4d8a8c202fdb2e6dc0fb3bfb0
Kim thanh <thanhtk2266@ut.edu.vn>
2026-01-14 10:38:04 +0700

    test branch

--- src/main/java/com/parking/system/config/SchedulerConfig.java
+++ src/main/java/com/parking/system/config/SchedulerConfig.java
@@ -8,3 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class SchedulerConfig {
     // Enable scheduling cho MonthlyTicketService
 }
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// NOTE: Do not remove or modify this file as it is essential for
+// enabling scheduling in the MonthlyTicketService.
